17f6aeb6b79bb89580b5759ebec7dddf10c01f7b
lukesaidel <95646517+lukesaidel@users.noreply.github.com>
2021-12-07 09:08:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -84,36 +84,35 @@ function exitPoints(exitText) {
     exitButton.classList.add('hide')
     richtigeProzent = 0
     if correctAnswersTest = 9 {
-        richtigeProzent = 100
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 100% Richtig")
     }
     if correctAnswersTest = 8 {
-        richtigeProzent = 88,889
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 88.889% Richtig")
     }
     if correctAnswersTest = 7 {
-        richtigeProzent = 77,778
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 77,778% Richtig")
     }
     if correctAnswersTest = 6 {
-        richtigeProzent = 66,667
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 66,667% Richtig")
     }
     if correctAnswersTest = 5 {
-        richtigeProzent = 55,556
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 55,556% Richtig")
     }
     if correctAnswersTest = 4 {
-        richtigeProzent = 44,44
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 44,44% Richtig")
     }
     if correctAnswersTest = 3 {
-        richtigeProzent = 33,33
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 33,33% Richtig")
     }
     if correctAnswersTest = 2 {
-        richtigeProzent = 22,22
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 22,22% Richtig")
     }
     if correctAnswersTest = 1 {
-        richtigeProzent = 11,11
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 11,11% Richtig")
     }
     if correctAnswersTest = 0 {
-        richtigeProzent = 0
+        alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, 0% Richtig")
     }
-    alert("Richtige Antworten: " + correctAnswersTest + " Anzahl der Fragen: 9, " + richtigeProzent + "%Richtig")
     startButton.innerText = 'Restart'
         startButton.classList.remove('hide')
 }
